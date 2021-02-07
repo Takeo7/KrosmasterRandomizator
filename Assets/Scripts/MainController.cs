ab252884fe428c48507adc1c53bffc4faa43a5ff
Takeo7 <arthur_wag@hotmail.com>
2021-02-07 18:13:55 +0100

    Imagenes Añadidas
    Imagenes Añadidas

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+
+    public MapGrid m_Grid;
+
+
+
+    public enum Spawn_Type
+    {
+        Krosmaster,
+        Monster,
+        Tree,
+        Mineral,
+        Dropable,
+        Market,
+        Zaap,
+        Craft,
+        Fenix
+    }
+}
