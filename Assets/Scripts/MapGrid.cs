48c0c7dd20ca527382621f4dad04df68e8282faf
Takeo7 <arthur_wag@hotmail.com>
2021-02-07 17:19:08 +0100

    First Commit
    Wolo

---
+++ Assets/Scripts/MapGrid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGrid : MonoBehaviour
+{
+    public GameObject Map;
+    public Transform[] Blocks;
+    public Transform[,] Grid;
+
+    void Start()
+    {
+        GetGrid(Map);
+    }
+
+    void GetGrid(GameObject Map)
+    {
+        int length = Map.transform.childCount - 1;
+        for (int i = 0; i < length; i++)
+        {
+            Blocks[i] = Map.transform.GetChild(i);
+        }
+
+        Grid = new Transform[6,6];
+
+        int count = 0;
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                Grid[i, j] = Blocks[count];
+                count++;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Scriptable/SC_Map.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Map_", menuName = "New Map")]
+public class SC_Map : ScriptableObject
+{
+    public string _name;
+    public Sprite map_Image;
+    public Vector2[] monster_spawns;
+    public Vector2[] tree_spawns;
+    public Vector2[] mineral_spawn;
+    public Vector2[] special_spawn;
+    public Vector2[] dropable_spawn;
+}
